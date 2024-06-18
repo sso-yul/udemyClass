05ddde790f5d8f7aea952e5eafa2255421949c24
sso-yul <ssoyul987@gmail.com>
2024-06-18 17:01:31 +0900

    플레이어1의 X와 플레이어2의 O를 번갈아가며 나타내기

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,20 +1,38 @@
+
+import { useState } from "react";
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ]
 
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
 
+    const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
 
-export default function GameBoard() {
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+
+            const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updateBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updateBoard;
+        });
+
+        onSelectSquare();
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button
+                                    onClick={() => handleSelectSquare(rowIndex, colIndex)}
+                                >
+                                    {playerSymbol}
+                                </button>
                             </li>
                         ))}
                     </ol>
