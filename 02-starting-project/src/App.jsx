4a8df5051ed851a072abb2480327317db541f4b7
sso-yul <ssoyul987@gmail.com>
2024-06-19 17:00:55 +0900

    헤더 추가

--- 02-starting-project/src/App.jsx
+++ 02-starting-project/src/App.jsx
@@ -1,6 +1,11 @@
+import Header from "./components/Header"
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header />
+      <h1>React Investment Calculator</h1>
+    </>
   )
 }
 
