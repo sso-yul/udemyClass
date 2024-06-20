55f66e5612515ddc5a3717f2b6b8ed7a5a256eb6
sso-yul <ssoyul987@gmail.com>
2024-06-20 19:00:54 +0900

    useState 활용법 배우기

--- 02-starting-project/src/App.jsx
+++ 02-starting-project/src/App.jsx
@@ -1,12 +1,42 @@
-import Header from "./components/Header"
+import { useState } from "react";
+import Header from "./components/Header.jsx"
+import UserInput from "./components/UserInput.jsx"
+import Results from "./components/Results.jsx";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <h1>React Investment Calculator</h1>
-    </>
-  )
+
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+    });
+
+    const inputIsValid = userInput.duration >= 1;
+
+    function handleChange(inputIdentifier, newValue) {
+        // 업데이트된 상태는 반드시 이전 상태를 기반으로 해야한다
+        setUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                // newValue 앞에 + 연산자를 추가하면서 문자열을 숫자로 변환하겠다 강제함
+                [inputIdentifier]: +newValue
+            };
+        });
+
+    }
+
+    return (
+        <>
+            <Header />
+            <UserInput
+                userInput={userInput}
+                onChange={handleChange}
+            />
+            {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+            { inputIsValid && <Results input={userInput} /> }
+        </>
+    )
 }
 
 export default App
--- 02-starting-project/src/components/Header.jsx
+++ 02-starting-project/src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/investment-calculator-logo.png";
 
 export default function Header() {
     return (
-        <header>
+        <header id="header">
             <img src={logo} alt="Logo showing a money bag" />
             <h1>Investment Calculator</h1>
         </header>
